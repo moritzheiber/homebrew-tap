d627e79ac133a83895aada9a6620950b3c7b98ea
Moritz Heiber <hello@heiber.im>
2021-10-31 13:57:15 +0100

    Updated brew formula

--- Formula/crowbar.rb
+++ Formula/crowbar.rb
@@ -2,22 +2,21 @@
 
 # The main formula
 class Crowbar < Formula
-  desc 'Your trusty tool for retrieving AWS credentials securely via SAML'
-  homepage 'https://github.com/moritzheiber/crowbar'
-  version '0.3.9'
-  bottle :unneeded
+  desc "Your trusty tool for retrieving AWS credentials securely via SAML"
+  homepage "https://github.com/moritzheiber/crowbar"
+  version "0.3.9"
 
   if OS.mac?
-    url 'https://github.com/moritzheiber/crowbar/releases/download/v0.3.9/crowbar-x86_64-macos'
-    sha256 '8c214925df64b3df6ab6f35e3f72a518a772d8d2a88a96277b8dc7b00e87149e'
+    url "https://github.com/moritzheiber/crowbar/releases/download/v0.3.9/crowbar-x86_64-macos"
+    sha256 "8c214925df64b3df6ab6f35e3f72a518a772d8d2a88a96277b8dc7b00e87149e"
   end
 
   def install
-    mv 'crowbar-x86_64-macos', 'crowbar'
-    bin.install 'crowbar'
+    mv "crowbar-x86_64-macos", "crowbar"
+    bin.install "crowbar"
   end
 
   test do
-    system "#{bin}/crowbar --version"
+    system "#{bin}/crowbar", "--version"
   end
 end
