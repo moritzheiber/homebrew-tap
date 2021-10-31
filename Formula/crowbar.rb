3dcf97068dca34fcb06224078b2748692d1ce0d2
Moritz Heiber <hello@heiber.im>
2021-10-31 14:22:16 +0100

    Lets build for macOS only

--- Formula/crowbar.rb
+++ Formula/crowbar.rb
@@ -5,11 +5,8 @@ class Crowbar < Formula
   desc "Your trusty tool for retrieving AWS credentials securely via SAML"
   homepage "https://github.com/moritzheiber/crowbar"
   version "0.3.9"
-
-  if OS.mac?
-    url "https://github.com/moritzheiber/crowbar/releases/download/v0.3.9/crowbar-x86_64-macos"
-    sha256 "8c214925df64b3df6ab6f35e3f72a518a772d8d2a88a96277b8dc7b00e87149e"
-  end
+  url "https://github.com/moritzheiber/crowbar/releases/download/v0.3.9/crowbar-x86_64-macos"
+  sha256 "8c214925df64b3df6ab6f35e3f72a518a772d8d2a88a96277b8dc7b00e87149e"
 
   def install
     mv "crowbar-x86_64-macos", "crowbar"
