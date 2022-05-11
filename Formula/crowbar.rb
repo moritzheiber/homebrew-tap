d1207da8ab18702d1b91b18415b0ab7cdfd0a8af
Moritz Heiber <hello@heiber.im>
2022-05-11 19:45:10 +0200

    Fix brew style issues

--- Formula/crowbar.rb
+++ Formula/crowbar.rb
@@ -4,8 +4,8 @@
 class Crowbar < Formula
   desc "Your trusty tool for retrieving AWS credentials securely via SAML"
   homepage "https://github.com/moritzheiber/crowbar"
-  version "0.4.0"
   url "https://github.com/moritzheiber/crowbar/releases/download/v0.4.0/crowbar-x86_64-macos"
+  version "0.4.0"
   sha256 "c97d25d73d585d403c6af615dddd2c66303b3f72ba04811b596281352f0234ec"
 
   def install
